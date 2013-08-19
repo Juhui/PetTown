ae69e13490cea5316b489a0924312ee9576b9448
KimWooHyun <dngus242@naver.com>
2013-08-19 12:51:45 +0900

    커밋 실험

---
+++ src/hs/GISC/PetTownActivity.java
@@ -0,0 +1,13 @@
+package hs.GISC;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PetTownActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
